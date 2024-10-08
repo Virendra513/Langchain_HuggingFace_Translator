4d97058767336aaa547ac60ba4d136009a49a123
Virendra <107255416+Virendra513@users.noreply.github.com>
2024-10-08 20:49:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,67 @@
 import streamlit as st
-from model import translate_text
+from model_python import translate_text
 
-st.title("English to Hindi Translator")
+
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #F0F2F5;  /* Background color */
+    }
+
+    .big-font {
+        font-size: 20px !important;
+        color: gray;
+         text-align: center;  /* Change to your desired color */
+    }
+    .custom-title {
+        font-size: 40px !important;
+        color: black;
+        text-align: center;
+        font-weight: bold;
+        text-decoration: underline;  /* Change to your desired color */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+st.markdown('<p class="custom-title">English to Hindi Translator</p>',unsafe_allow_html=True)
+st.write("")
 
 # User input
-text_to_translate = st.text_area("Enter text in English:")
+text_to_translate = st.text_area("Enter/Paste text in English:")
 
 # Button to trigger translation
 if st.button("Translate"):
     if text_to_translate:
         # Call the translation function
         output = translate_text(text_to_translate)
-        st.write("Translation in Hindi:", output)
+        st.write("Translation in Hindi Language is :", output['text'])
     else:
-        st.error("Please enter some text to translate.")
+        st.error("Please enter/paste valid to translate.")
+
+st.write("")  # Adds an empty line
+st.write("")  # Adds an empty line
+st.write("")  # Adds an empty line
+st.write("")  # Adds an empty line
+st.write("")  # Adds an empty line
+st.write("")  # Adds an empty line
+st.write("")  # Adds an empty line
+st.write("")  # Adds an empty line
+st.write("")  # Adds an empty line
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("") 
+st.write("")  
+
+st.markdown('<p class="big-font">Designed and Developed by Virendra S    @2024</p>', unsafe_allow_html=True)
